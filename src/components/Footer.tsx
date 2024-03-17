90c3b8e179d7eb4dfcecc1984475d690e22a3b53
imtithal <halaloimtithal@gmail.com>
2024-03-18 01:00:44 +0200

    adding the footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className={'border-top py-4 bg-dark fixed-bottom'}>
+            <div className="container-lg">
+                <p className="text-center text-white">
+                    &copy; reserved to AlphaStore 2024
+                </p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
